4619926e18e956d12d475edc47c13f142f38c2a7
Egor Doudkin <egor.doudkin@xaxis.com>
2018-08-01 16:21:32 +0200

    circleci eval

---
+++ dependent1/src/main/java/edoudkin/samples/App.java
@@ -0,0 +1,13 @@
+package edoudkin.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
